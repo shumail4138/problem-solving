290f29a3426c0958da867b67c44d576f33249581
Shumail Rahat Khan <113338048+shumail4138@users.noreply.github.com>
2024-09-27 23:20:08 +0530

    Update k_sized_subarray_maximum.cpp

--- k_sized_subarray_maximum.cpp
+++ k_sized_subarray_maximum.cpp
@@ -1,1 +1,30 @@
-
+vector<int> max_of_subarrays(int k, vector<int> &arr)
+{
+    vector<int> ans;
+    int n = arr.size();
+    int i = 0;
+    int j = 0;
+    int maxi = -1;
+    while (j < n)
+    {
+        if (arr[j] > maxi)
+        {
+            maxi = arr[j];
+        }
+        if (j - i + 1 == k)
+        {
+            ans.push_back(maxi);
+            if (arr[i] == maxi)
+            {
+                maxi = -1;
+                j = i;
+            }
+            i++;
+        }
+        if (j - i + 1 != k)
+        {
+            j++;
+        }
+    }
+    return ans;
+}
