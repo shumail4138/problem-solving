8420ae876a08440839d6bb36c9aaaa3060d3bcad
Shumail Rahat Khan <113338048+shumail4138@users.noreply.github.com>
2024-09-28 23:13:16 +0530

    Update smallest_window_in_a_string_containing_all_the_characters_of_another_string.cpp

--- smallest_window_in_a_string_containing_all_the_characters_of_another_string.cpp
+++ smallest_window_in_a_string_containing_all_the_characters_of_another_string.cpp
@@ -1,1 +1,41 @@
 
+string smallestWindow(string s, string p)
+{
+    int min_len = INT_MAX;
+    vector<int> freq(26, 0);
+    for (char &ch : p)
+    {
+        freq[ch - 'a']++;
+    }
+    int countReq = p.size();
+    int i = 0;
+    int j = 0;
+    int start_i = 0;
+    while (j < s.size())
+    {
+        char ch = s[j];
+        if (freq[ch - 'a'] > 0)
+        {
+            countReq--;
+        }
+        freq[ch - 'a']--;
+        while (countReq == 0)
+        {
+            int window = j - i + 1;
+            if (min_len > window)
+            {
+                min_len = window;
+                start_i = i;
+            }
+            char ch = s[i];
+            freq[ch - 'a']++;
+            if (freq[ch - 'a'] > 0)
+            {
+                countReq++;
+            }
+            i++;
+        }
+        j++;
+    }
+    return min_len == INT_MAX ? "-1" : s.substr(start_i, min_len);
+}
